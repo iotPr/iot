d64fa1ebae2a16e06a346e40cdea2b572d8ff957
Maimon <yagel.maimon@intel.com>
2024-07-16 18:18:06 +0300

    delete token

--- Scripts/send_msg_to_gpt.cpp
+++ Scripts/send_msg_to_gpt.cpp
@@ -1,8 +1,9 @@
 
-
+// sk-None-NWc1XdBugZqGlL46oYVhT3BlbkFJ8ornaGsYwuZsSDljtReU
 const char* ssid = "Yag";
 const char* password = "0545624950";
-const char* apiKey = "sk-None-NWc1XdBugZqGlL46oYVhT3BlbkFJ8ornaGsYwuZsSDljtReU";
+
+
 void sendTextToOpenAI(String inputText);
 void setup() {
   Serial.begin(115200);
