4d09e52b98e617412906a8bd643ba3282ea03161
Gilad <yoav.gilad@intel.com>
2024-08-18 20:41:08 +0300

    adding api key

--- src/speech_to_text/network_param.h
+++ src/speech_to_text/network_param.h
@@ -49,7 +49,7 @@ const char* root_ca=
 // Google recommends to use Access Token.
 
 // It is also possible to use "API Key" instead of "Access Token". It doesn't have time limit.
-const String ApiKey = "";
+const String ApiKey = "AIzaSyAIgezZitJr_mpSK-7bkC7GPstBSTiR9-M";
 
 // see https://cloud.google.com/docs/authentication?hl=ja#getting_credentials_for_server-centric_flow
 // see https://qiita.com/basi/items/3623a576b754f738138e (Japanese)
